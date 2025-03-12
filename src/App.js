5f99e83e3cb9a7f702fbe2245318c36dff5325af
“Brittany <brittany@signaturegroupevents.com>
2025-03-12 14:35:29 -0400

    Started base and Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import TheoryList from './components/TheoryList'
+import TheoryForm from './components/TheoryForm'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <nav>
+      <Link to='/'>Theories</Link>
+      <Link to='/add'>Add Theory</Link>
+    </nav>
+      <Routes path='/' element={TheoryList} />
+      <Routes path='/add' element={TheoryForm} />
+   </Router>
   );
 }
 
